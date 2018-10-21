31bef55612a7be70265f97a07a12c6c6d5a800f7
Glen Davies <glendaviesnz@gmail.com>
2018-10-22 11:29:32 +1300

    Add some responsive styling

--- src/TalkList.js
+++ src/TalkList.js
@@ -5,52 +5,30 @@ import { css } from 'react-emotion';
 import TalkListItem from './TalkListItem';
 import { config } from './config';
 
-const TalkListTable = css`
-  width: 95%;
-  table-layout: fixed;
+const list = css`
+  max-width: 1200px;
+  padding: 24px;
   margin: auto
 `;
 
-const tableHeader = css`
-  text-align: left;
-`;
-
-const title = css`
-  width: 60%;
-  padding: 4px;
-`;
-
-const speaker = css`
-  width: 20%;
-  padding: 4px;
-`;
-
-const date = css`
-  width: 20%;
-  padding: 4px;
-`;
-
 class TalkList extends Component {
-  state = { TalkList: [] }
+  state = { talkList: [] }
 
   componentDidMount() {
     axios.get(config.apiUrl)
       .then(response => {
-        this.setState({ TalkList: response.data })
+        this.setState({ talkList: response.data })
       });
   }
 
   render() {
-    var TalkList = this.state.TalkList.map(function (Talk) {
-      return <TalkListItem key={Talk.id} Talk={Talk} />;
+    var talkList = this.state.talkList.map(function (talk) {
+      return <TalkListItem key={talk.id} Talk={talk} />;
     })
 
-    return <table className={TalkListTable}>
-    <tbody>
-      <tr className={tableHeader}><th className={title}>Title</th><th className={speaker}>Speaker</th><th className={date}>Created</th></tr>
-      
-      {TalkList}
-    </tbody></table>;
+    return <ul className={list}>
+      {talkList}
+    </ul>
   }
 }
 
--- src/TalkListItem.js
+++ src/TalkListItem.js
@@ -2,46 +2,70 @@ import * as React from 'react';
 import { css } from 'react-emotion';
 import { Link } from "react-router-dom";
 
+const talkListItem = css`
+  display: grid;
+  grid-gap: 2px;
+  grid-template-columns: 1fr 1fr;
+  margin-bottom: 8px;
+  border: 1px solid #cccccc;
+  border-radius: 2px;
+  padding: 8px;
+  &:hover {
+    background-color: grey;
+  }
+
+  @media (min-width: 46em) {
+    grid-template-columns: 2fr 1fr 1fr;
+    border: 0px;
+    border-radius: 0px;
+  }
+`;
 const title = css`
-  width: 60%;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  padding: 4px;
+
+  @media (max-width: 46em) {
+    grid-column: 1/3;
+    font-weight: 600;
+  }
 `;
 
 const speaker = css`
-  width: 20%;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  padding: 4px;
 `;
 
+const talkLink = css`
+    text-decoration: none;
+    color: black;
+`;
 const date = css`
-  width: 20%;
-  padding: 4px;
+    text-align: right;
 `;
 
 
-const TalkListItem = ({Talk}) => {
-    console.log(Talk);
-    return (
-    <tr>
-      <td className={title}><Link to={`/details/${Talk.id}`}>{ Talk.title }</Link></td>
-      <td className={speaker}>{ Talk.speaker }</td>
-      <td className={date}>{ formatDate(Talk.date_entered ) }</td>
-    </tr>
+const TalkListItem = ({ Talk }) => {
+  // console.log(Talk);
+  return (
+    <Link to={`/details/${Talk.id}`} className={talkLink}>
+    <div className={talkListItem}>
+      <div className={title}>{Talk.title}</div>
+      <div className={speaker}>{Talk.speaker}</div>
+      <div className={date}>{formatDate(Talk.date_entered)}</div>
+    </div>
+    </Link>
   );
 };
 
 export default TalkListItem;
 
-function formatDate(date)  {
+function formatDate(date) {
 
-    const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-    const dateParts = date.split('-');
+  const dateParts = date.split('-');
 
-    return `${shortMonths[dateParts[1] - 1]} ${dateParts[0]}`;
+  return `${shortMonths[dateParts[1] - 1]} ${dateParts[0]}`;
 }
